746061b2dbfc996eff0f3a8ce6051ea4f5606952
Selifanchik <selifanchik@mail.ru>
2023-10-09 23:22:32 +0300

    save changes

--- apps/bot.py
+++ apps/bot.py
@@ -4,6 +4,7 @@ from telegram import Update, ReplyKeyboardRemove
 from telegram.ext import CommandHandler, ContextTypes, ConversationHandler, MessageHandler, filters
 from apps.constants import TOKENWEATHERAPI
 from apps.weatherapi import Requests
+from apps.weatherconst import WINDDIR
 
 CITY = 0
 
@@ -25,6 +26,11 @@ class BotConversation:
         await context.bot.send_message(chat_id=update.effective_chat.id, text="Хотите узнать погоду?")
         await update.message.reply_text('Какой город Вас интересует')
         return CITY
+    
+
+    @staticmethod
+    async def customweather(update: Update, context: ContextTypes.DEFAULT_TYPE):
+        await update.message.reply_text('Bla-bla')
 
 
 
@@ -51,7 +57,24 @@ class BotConversation:
             print(data.json())
             town = data.json()['location']['name']
             temp = data.json()['current']['temp_c']
-            await update.message.reply_text(f'Температура воздуха в городе {town} составляет {temp} °C')
+            feelslike_temp = data.json()['current']['feelslike_c']
+            condition = data.json()['current']['condition']['text']
+            wind = data.json()['current']['wind_kph'] * 1000 / 3600
+            wind_gust = data.json()['current']['gust_kph'] * 1000 / 3600
+            wind_dir = WINDDIR[data.json()['current']['wind_dir']]
+            pressure_mb = data.json()['current']['pressure_mb'] / 1.333
+            precip_mm = data.json()['current']['precip_mm']
+            humidity = data.json()['current']['humidity']
+            await update.message.reply_text(f'Погода в городе {town}:\n\
+\n\
+{condition} {temp} °C\n\
+(ощущается как {feelslike_temp}°C)\n\
+Ветер {wind_dir} {round(wind, 1)} м/c, порывы до {round(wind_gust, 1)} м/c\n\
+Давление {round(pressure_mb, 1)} мм рт. ст.\n\
+Осадки {precip_mm} мм\n\
+Влажность {humidity}%')
+
+
         else:
             await update.message.reply_text(f'Проверьте правильность написания города {text}')
         return ConversationHandler.END
